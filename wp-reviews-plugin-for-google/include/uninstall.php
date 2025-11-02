2eb65c3eb83b44baf21515963ce40ea8daf5c82b
0xMokusou <mokusou.recon@gmail.com>
2025-11-02 20:06:58 +0900

    13.2.2

---
+++ wp-reviews-plugin-for-google/include/uninstall.php
@@ -0,0 +1,13 @@
+<?php
+defined('ABSPATH') or die('No script kiddies please!');
+foreach ($this->get_option_names() as $optName) {
+delete_option($this->get_option_name($optName));
+}
+global $wpdb;
+$tiReviewsTableName = $this->get_tablename('reviews');
+$tiViewsTableName = $this->get_tablename('views');
+include $this->get_plugin_dir() . 'include' . DIRECTORY_SEPARATOR . 'schema.php';
+foreach (array_keys($ti_db_schema) as $tableName) {
+$wpdb->query($wpdb->prepare('DROP TABLE IF EXISTS %i', $this->get_tablename($tableName)));
+}
+?>
